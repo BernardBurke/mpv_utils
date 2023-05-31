485968635e7e50866c988d25220d6821aa90ac5c
Ben Burke <bernard.francis.burke@gmail.com>
2023-05-31 15:29:44 +1000

    adding the yt-dlp bits

--- snatch_audio.sh
+++ snatch_audio.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 # use FFMpeg to copy out the audio stream from a video file
-source $MPVU/util_inc.sh 
+source $MPVU/util_inc.sh
+
+TMPFILE1=$(mktemp)
+
 
 if [[ "$1" == "" ]]; then
-    message "Please specify a video file"
+    message "Please specify youtube/rumble url"
     exit 1
-else
-    if [[ ! -f "$1" ]]; then
-        message "$1 does not appear to be a video file"
-        exit 1
-    fi
 fi 
 
-BARE_NAME="$1"
-DIRNAME="$(dirname "$1")"
-BASE_NAME=$(basename -- "$1")
+if yt-dlp -f mp4-240p "$1"; then
+    message "Downloaded $1"
+else
+    message "Download failed - probably an unsupported video format"
+fi
+
+BARE_NAME="$TMPFILE1"
+DIRNAME="$(dirname "$TMPFILE1")"
+BASE_NAME=$(basename -- "$TMPFILE1")
 FILE_EXTENSION="${BASE_NAME##*.}"
 FILE_NAME="${BASE_NAME%.*}"
 #ALPH_NAME="${FILE_NAME//[^[:alnum:]_-]/}" 
@@ -34,3 +38,4 @@ TARGET_AUDIO="$DIRNAME/$OUTFILENAME.$AUDIO_FORMAT"
 message "AudioFormat: $AUDIO_FORMAT target filename: $TARGET_AUDIO"
 
 ffmpeg -i "$BARE_NAME" -vn -acodec copy "$TARGET_AUDIO"
+message "Audio produced: $AUDIO_FORMAT target filename: $TARGET_AUDIO
