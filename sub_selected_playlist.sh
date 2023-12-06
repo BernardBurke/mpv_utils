7cabc88191453b9b9bbc39e77cacb8cc054066a6
Ben Burke <bernard.francis.burke@gmail.com>
2023-12-06 19:26:17 +1100

    max rows

--- sub_selected_playlist.sh
+++ sub_selected_playlist.sh
@@ -15,6 +15,7 @@ else
         VOLUME=$3
 fi
 
+
 TMPFILE1=$(mktemp)
 TMPFILE2=$(mktemp)
 TMPFILE3=$(mktemp)
@@ -27,11 +28,20 @@ sed -e 's/.vtt-/.mp4\" --start=/g' -i $TMPFILE2
 sed -e 's/\/mnt/\"\/mnt/g' -i $TMPFILE2
 tr -d '\r' <$TMPFILE2 > $TMPFILE1
  
+ROW_COUNT=0
+MAXROWS=10000
 
 echo "mpv --profile=override --volume=$VOLUME --screen=$SCREEN \\" > $TMPFILE3
 while read -r pss; do 
-	echo -e  "--\{ $pss --\} \\"  >> $TMPFILE3
+        echo -e  "--\{ $pss --\} \\"  >> $TMPFILE3
+        echo -e  "--\{ $pss --\} \\"  >> $USCR/sub_selected_$$.txt
+        ((ROW_COUNT=ROW_COUNT+1))
+        if [[ ROW_COUNT -gt MAXROWS ]]; then 
+                echo "Maxed out at $MAXROWS - continuing"
+                break
+        fi
 done < "$TMPFILE1" 
+read -p "Press return $ROW_COUNT"
 
 cp $TMPFILE3 /tmp/two.sh
 
