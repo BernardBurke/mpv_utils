1aecb41883ebb2735b089396e9f60f1b4955e21b
Ben Burke <bernard.francis.burke@gmail.com>
2024-02-13 06:18:16 +1100

    smoothing

--- sub_selected_playlist.sh
+++ sub_selected_playlist.sh
@@ -23,7 +23,7 @@ fi
 
 
 if [[ "$5" == "" ]]; then
-        AFTER=4
+        AFTER=10
 else
         AFTER=$5
 fi
@@ -34,12 +34,14 @@ TMPFILE1=$(mktemp)
 TMPFILE2=$(mktemp)
 TMPFILE3=$(mktemp)
 
-find $GRLSRC -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru > $TMPFILE1
-find $GRLSRC2 -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru >> $TMPFILE1
+#find $GRLSRC -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru > $TMPFILE1
+#find $GRLSRC2 -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru >> $TMPFILE1
+find $GRLSRC -type f -iname '*.vtt' -exec grep -iH -B $BEFORE  "$1" "{}" \; | sort -Ru > $TMPFILE1
+find $GRLSRC2 -type f -iname '*.vtt' -exec grep -iH -B $BEFORE  "$1" "{}" \; | sort -Ru >> $TMPFILE1
 #cat $TMPFILE1
 grep ".vtt-00" $TMPFILE1 > $TMPFILE2
 #sed -e 's/> /end=/g' -i $TMPFILE2
-sed -e 's/>.*/length=10/g' -i $TMPFILE2
+sed -e "s/>.*/length=$AFTER/g" -i $TMPFILE2
 sed -e 's/.vtt-/.mp4\" --start=/g' -i $TMPFILE2
 sed -e 's/\/mnt/\"\/mnt/g' -i $TMPFILE2
 sed -e 's/\/home/\"\/home/g' -i $TMPFILE2
@@ -47,11 +49,14 @@ tr -d '\r' <$TMPFILE2 > $TMPFILE1
 
 cat $TMPFILE1 | sort -Ru > $TMPFILE2
 cp $TMPFILE2    $TMPFILE1
- 
+cat $TMPFILE1
+
 ROW_COUNT=0
 MAXROWS=10000
 
 echo "mpv --profile=override --volume=$VOLUME --screen=$SCREEN \\" > $TMPFILE3
+echo "mpv --profile=override --volume=$VOLUME --screen=$SCREEN \\" > $USCR/sub_selected_$$.txt
+
 while read -r pss; do 
         echo -e  "--\{ $pss --\} \\"  >> $TMPFILE3
         echo -e  "--\{ $pss --\} \\"  >> $USCR/sub_selected_$$.txt
