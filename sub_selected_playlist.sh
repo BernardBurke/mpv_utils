cf2933b553e79bd13550ccc7f82b3da008a5d709
Ben Burke <bernard.francis.burke@gmail.com>
2024-02-13 05:45:16 +1100

    smoothing

--- sub_selected_playlist.sh
+++ sub_selected_playlist.sh
@@ -15,18 +15,38 @@ else
         VOLUME=$3
 fi
 
+if [[ "$4" == "" ]]; then
+        BEFORE=12
+else
+        BEFORE=$4
+fi
+
+
+if [[ "$5" == "" ]]; then
+        AFTER=4
+else
+        AFTER=$5
+fi
+
+
 
 TMPFILE1=$(mktemp)
 TMPFILE2=$(mktemp)
 TMPFILE3=$(mktemp)
 
-find $GRLSRC -type f -iname '*.vtt' -exec grep -iH -B 12 -A 4 "$1" "{}" \; | sort -Ru > $TMPFILE1
+find $GRLSRC -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru > $TMPFILE1
+find $GRLSRC2 -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru >> $TMPFILE1
+#cat $TMPFILE1
 grep ".vtt-00" $TMPFILE1 > $TMPFILE2
 #sed -e 's/> /end=/g' -i $TMPFILE2
 sed -e 's/>.*/length=10/g' -i $TMPFILE2
 sed -e 's/.vtt-/.mp4\" --start=/g' -i $TMPFILE2
 sed -e 's/\/mnt/\"\/mnt/g' -i $TMPFILE2
+sed -e 's/\/home/\"\/home/g' -i $TMPFILE2
 tr -d '\r' <$TMPFILE2 > $TMPFILE1
+
+cat $TMPFILE1 | sort -Ru > $TMPFILE2
+cp $TMPFILE2    $TMPFILE1
  
 ROW_COUNT=0
 MAXROWS=10000
