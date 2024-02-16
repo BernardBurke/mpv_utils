2243ec70af2a032f785b67b695a917f604f6fd39
Ben Burke <bernard.francis.burke@gmail.com>
2024-02-16 19:05:12 +1100

    whats possible with SG filenames

--- sub_selected_audio.sh
+++ sub_selected_audio.sh
@@ -36,18 +36,19 @@ TMPFILE3=$(mktemp)
 
 #find $GRLSRC -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru > $TMPFILE1
 #find $GRLSRC2 -type f -iname '*.vtt' -exec grep -iH -B $BEFORE -A $AFTER "$1" "{}" \; | sort -Ru >> $TMPFILE1
-find $GRLSRC -type f -iname '*.vtt' -exec grep -iH -B $BEFORE  "$1" "{}" \; | sort -Ru > $TMPFILE1
-find $GRLSRC2 -type f -iname '*.vtt' -exec grep -iH -B $BEFORE  "$1" "{}" \; | sort -Ru >> $TMPFILE1
+find $AUDEY -type f -iname '*.srt' -exec grep -iH -B $BEFORE  "$1" "{}" \; | sort -Ru > $TMPFILE1
+find $AUDEY2 -type f -iname '*.srt' -exec grep -iH -B $BEFORE  "$1" "{}" \; | sort -Ru >> $TMPFILE1
 #cat $TMPFILE1
-grep ".vtt-00" $TMPFILE1 > $TMPFILE2
+grep ".srt-00" $TMPFILE1 | grep -v '"' | grep -v "'" | grep -v "\[" | grep -v the-doctors | shuf -n 500 > $TMPFILE2
 #sed -e 's/> /end=/g' -i $TMPFILE2
 sed -e "s/>.*/length=$AFTER/g" -i $TMPFILE2
-sed -e 's/.vtt-/.mp4\" --start=/g' -i $TMPFILE2
+sed -e 's/.srt-/.m4a\" --start=/g' -i $TMPFILE2
+sed -e 's/,/./g' -i $TMPFILE2
 sed -e 's/\/mnt/\"\/mnt/g' -i $TMPFILE2
 sed -e 's/\/home/\"\/home/g' -i $TMPFILE2
 tr -d '\r' <$TMPFILE2 > $TMPFILE1
 
-cat $TMPFILE1 | sort -Ru > $TMPFILE2
+cat $TMPFILE1 | sort -Ru | grep -v "(" > $TMPFILE2
 cp $TMPFILE2    $TMPFILE1
 cat $TMPFILE1
 
@@ -55,11 +56,11 @@ ROW_COUNT=0
 MAXROWS=10000
 
 echo "mpv --profile=override --volume=$VOLUME --screen=$SCREEN \\" > $TMPFILE3
-echo "mpv --profile=override --volume=$VOLUME --screen=$SCREEN \\" > $USCR/sub_selected_$$.txt
+echo "mpv --profile=override --volume=$VOLUME --screen=$SCREEN \\" > $USCR/sub_selected_audio_$$.txt
 
 while read -r pss; do 
         echo -e  "--\{ $pss --\} \\"  >> $TMPFILE3
-        echo -e  "--\{ $pss --\} \\"  >> $USCR/sub_selected_$$.txt
+        echo -e  "--\{ $pss --\} \\"  >> $USCR/sub_selected_audio_$$.txt
         ((ROW_COUNT=ROW_COUNT+1))
         if [[ ROW_COUNT -gt MAXROWS ]]; then 
                 echo "Maxed out at $MAXROWS - continuing"
@@ -68,7 +69,7 @@ while read -r pss; do
 done < "$TMPFILE1" 
 read -p "Press return $ROW_COUNT"
 
-cp $TMPFILE3 /tmp/two.sh
+cp $TMPFILE3 /tmp/three.sh
 
-nohup bash -x /tmp/two.sh &
+#nohup bash -x /tmp/three.sh &
 
