0a251efa828eed8db9412469bb1d6df0d6f2b033
Ben Burke <bernard.francis.burke@gmail.com>
2024-07-03 04:54:22 +1000

    whis missing and video shift

--- shift_subtitles.py
+++ shift_subtitles.py
@@ -77,46 +77,35 @@ def convert_timestamp_to_milliseconds(timestamp_str):
         raise ValueError("Invalid timestamp format. Use HH:MM:SS")
 
 
-# This function takes the .srt/vtt file and finds the related audio file in the same directory
-def get_audio_file(input_file):
-    audio_file = None
-    # Look for a related audio file in the same directory as the input subtitle file
-    # File types are .mp3, m4a, .wav, .flac, .ogg, .opus .webm .mp4, .mkv
-    # For example, if the input file is "subtitle.srt", the audio file would be "subtitle.mp3" if the 
+# This function takes the .srt/vtt file and finds the related media file in the same directory
+def get_media_file(input_file):
+    media_file = None
+    # Look for a related media file in the same directory as the input file
+    # File types can be media or video (mp3, m4a, wav, flac, ogg, opus, webm, mp4, mkv, avi, etc.)
+    # For example, if the input file is "subtitle.srt", the media file would be "subtitle.mp3" if the 
     # mp3 file exists (or m4a if the m4a file exists, etc.)
-    # So, take the input file, remove the .srt extension and try each audio file extension
+    # So, take the input file, remove the extension and try each media file extension
     # and check if that file exists.
-    # If the audio file is found, return the audio file name. If not found, return None.
-    input_file_without_extension = input_file.replace(".srt", "").replace(".vtt", "")
+    # If the media file is found, return the media file name. If not found, return None.
+    input_file_without_extension = os.path.splitext(input_file)[0]
     input_file_directory = os.path.dirname(input_file)
-    # for each audio file extension, check if the file exists
-    # make a list of audio file types .mp3, m4a, .wav, .flac, .ogg, .opus .webm .mp4, .mkv
-    audio_file_extensions = [".mp3", ".m4a", ".wav", ".flac", ".ogg", ".opus", ".webm", ".mp4", ".mkv", ".aac"]
-    for extension in audio_file_extensions:
-        audio_file = f"{input_file_without_extension}{extension}"
-        if os.path.exists(audio_file):
-            # check in internal type of the audio file and fixup the extension if necessary
-            # the fixup involves copying the audio file to /tmp with a new extension that
-            # matches the internal type
-            audio_type = get_audio_type(audio_file)
-            if extension != f".{audio_type}":
-                new_audio_file = f"{input_file_without_extension}.{audio_type}"
-                print(f"Fixing audio file extension: {audio_file} to {new_audio_file}")
-                new_audio_file = f"/tmp/{os.path.basename(new_audio_file)}"
-                os.system(f"cp '{audio_file}' '{new_audio_file}'")
-                audio_file = new_audio_file
+    # List of media file extensions
+    media_file_extensions = [".mp3", ".m4a", ".wav", ".flac", ".ogg", ".opus", ".webm", ".mp4", ".mkv", ".avi"]
+    for extension in media_file_extensions:
+        media_file = f"{input_file_without_extension}{extension}"
+        if os.path.exists(media_file):
             break
         else:
-            audio_file = None
-    return audio_file
+            media_file = None
+    return media_file
 
 import ffmpeg
 
-def extract_audio_segment(input_file, start_time, end_time, output_file):
-    """Extracts a segment of audio from an input file without re-encoding.
+def extract_media_segment(input_file, start_time, end_time, output_file):
+    """Extracts a segment of media (audio or video) from an input file without re-encoding.
 
     Args:
-        input_file (str): Path to the input audio file.
+        input_file (str): Path to the input media file.
         start_time (str): Start time in HH:MM:SS format.
         end_time (str): End time in HH:MM:SS format.
         output_file (str): Path to save the extracted segment.
@@ -129,9 +118,9 @@ def extract_audio_segment(input_file, start_time, end_time, output_file):
             .output(output_file, c='copy')
             .run()
         )
-        print(f"Audio segment successfully extracted to {output_file}")
+        print(f"Media segment successfully extracted to {output_file}")
     except ffmpeg.Error as e:
-        print(f"Error extracting audio segment: {e.stderr}")
+        print(f"Error extracting media segment: {e.stderr}")
 
 
 
@@ -155,7 +144,7 @@ if __name__ == "__main__":
     parser.add_argument("input_file", help="Input subtitle file (.srt or .vtt)")
     # parser.add_argument("output_file", help="Output subtitle file")
     parser.add_argument("time_offset", help="Time offset in HH:MM:SS format (e.g., 00:01:30)")
-    parser.add_argument("-l","--audio_length", type=int, help="Audio length in seconds")
+    parser.add_argument("-l","--media_length", type=int, help="Media length in seconds")
 
     args = parser.parse_args()
 
@@ -171,24 +160,39 @@ if __name__ == "__main__":
     output_directory = os.environ['PERE']
     
 
-    # call get_audio_file function to get the audio file name. If it returns a valid filename,
+    # if the input file is .vtt, use ffmpeg to convert it to .srt and save in the same directory as .vtt
+    if args.input_file.endswith('.vtt'):
+        output_srt_file = f"{os.path.splitext(args.input_file)[0]}.srt"
+        print(f"Converting {args.input_file} to {output_srt_file}")
+        try:
+            (
+                ffmpeg
+                .input(args.input_file)
+                .output(output_srt_file)
+                .run()
+            )
+            print(f"Subtitle file successfully converted to {output_srt_file}")
+        except ffmpeg.Error as e:
+            print(f"Error converting subtitle file: {e.stderr}")
+        args.input_file = output_srt_file
+    # call get_media_file function to get the media file name. If it returns a valid filename,
     # then continue, otherwise print an error and exit
-    audio_file = get_audio_file(args.input_file)
-    if audio_file is None:
-        print("Could not find the related audio file.")
+    media_file = get_media_file(args.input_file)
+    if media_file is None:
+        print("Could not find the related media file.")
         sys.exit(1)
-    # get the length of the audio file in milliseconds
-    audio_file_info = ffmpeg.probe(audio_file)
-    audio_duration = int(float(audio_file_info['format']['duration']) * 1000)
-    #audio_duration = int(float(audio_file_info['format']['duration']))
-    print(f"Audio File: {audio_file}  duration: {audio_duration} ms")
-    # Calculate the time difference between the audio duration and the time_offset
+    # get the length of the media file in milliseconds
+    media_file_info = ffmpeg.probe(media_file)
+    media_duration = int(float(media_file_info['format']['duration']) * 1000)
+    #media_duration = int(float(media_file_info['format']['duration']))
+    print(f"media File: {media_file}  duration: {media_duration} ms")
+    # Calculate the time difference between the media duration and the time_offset
     time_offset = convert_timestamp_to_milliseconds(args.time_offset)
     print(f"Time offset: {time_offset} ms")
-    if args.audio_length:
-        time_difference = time_offset + args.audio_length * 1000
-        if time_difference > audio_duration:
-            print("Cut length is greater than the audio duration.")
+    if args.media_length:
+        time_difference = time_offset + args.media_length * 1000
+        if time_difference > media_duration:
+            print("Cut length is greater than the media duration.")
             sys.exit(1)
         else:
             print(f"Time difference: {time_difference} ms") 
@@ -197,25 +201,25 @@ if __name__ == "__main__":
             print(f'Time difference: {time_difference}')
             input("Press Enter to continue...")
     else:
-        time_difference = audio_duration - time_offset
+        time_difference = media_duration - time_offset
         print(f"Time difference: {time_difference} ms")
         # if the time difference is negative, print an error and exit
         if time_difference < 0:
-            print("Time offset is greater than the audio duration.")
+            print("Time offset is greater than the media duration.")
             sys.exit(1)
 
-# extract the audio sections - destination is in /tmp/ with filename the same as the audio_file
-    audio_output = f"{output_directory}/{os.path.basename(audio_file)}"
-    print(f"Extracting audio file: {audio_file} from {args.time_offset} to {audio_output}")
+# extract the media sections - destination is in /tmp/ with filename the same as the media_file
+    media_output = f"{output_directory}/{os.path.basename(media_file)}"
+    print(f"Extracting media file: {media_file} from {args.time_offset} to {media_output}")
 
     time_offset_formatted = format_time_offset(args.time_offset)
-    cut_length_suffix = f"-cut{args.audio_length}" if args.audio_length else ""
-    audio_output = f"{output_directory}/{os.path.basename(audio_file).rsplit('.', 1)[0]}_{time_offset_formatted}{cut_length_suffix}.{audio_file.rsplit('.', 1)[1]}"  
+    cut_length_suffix = f"-cut{args.media_length}" if args.media_length else ""
+    media_output = f"{output_directory}/{os.path.basename(media_file).rsplit('.', 1)[0]}_{time_offset_formatted}{cut_length_suffix}.{media_file.rsplit('.', 1)[1]}"  
     #output_srt_file = f"{output_directory}/{os.path.basename(args.input_file).rsplit('.', 1)[0]}_{time_offset_formatted}{cut_length_suffix}.srt" 
     output_srt_file = f"{output_directory}/{os.path.basename(args.input_file).rsplit('.', 1)[0]}_{time_offset_formatted}{cut_length_suffix}.{args.input_file.rsplit('.', 1)[1]}"  
-    print(f"Audio output file: {audio_output} output_srt file: {output_srt_file}")
+    print(f"media output file: {media_output} output_srt file: {output_srt_file}")
 
-    extract_audio_segment(audio_file, args.time_offset, time_difference, audio_output)
+    extract_media_segment(media_file, args.time_offset, time_difference, media_output)
 
     # output_srt_file = f"{output_directory}/{os.path.basename(args.input_file)}"
     shift_subtitles(args.input_file, output_srt_file, args.time_offset)
