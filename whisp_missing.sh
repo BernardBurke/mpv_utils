e690ef44f41d3a2a6bc9a0cec9dcfcff84950f3f
Ben Burke <bernard.francis.burke@gmail.com>
2024-07-30 08:41:43 +1000

    create an input file mode

--- whisp_missing.sh
+++ whisp_missing.sh
@@ -14,10 +14,18 @@ fi
 
 path="$1"
 
+
 # Find all video and audio files, ignoring case (e.g., .MP4, .mKV, etc.)
 counter=0
-find "$path" -type f \( -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" -o -iname "*.mov" -o -iname "*.webm" -o -iname "*.mp3" -o -iname "*.wav" -o -iname "*.m4a" \) | while read file; do
+tempfile="$(mktemp)"
 
+if [[ -f "$path" ]]; then
+  cp "$path" "$tempfile"
+else
+  find "$path" -type f \( -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" -o -iname "*.mov" -o -iname "*.webm" -o -iname "*.mp3" -o -iname "*.wav" -o -iname "*.m4a" \)  > "$tempfile"
+fi
+
+while read file; do
   counter=$((counter + 1))
   if [ $counter -gt $loop_count ]; then
     break
@@ -28,6 +36,6 @@ find "$path" -type f \( -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" -o -i
   lpath="$(dirname "$file")"
   # Check if a .srt or .vtt file exists with the same base name
   if [ ! -f "$lpath/$base.srt" ] && [ ! -f "$lpath/$base.vtt" ]; then
-    echo "$MPVU/whisp_me.sh $file"  # Output the file if no subtitles are found
+    echo "$MPVU/whisp_me.sh \"$file\""  # Output the file if no subtitles are found
   fi
-done
+done < "$tempfile"
