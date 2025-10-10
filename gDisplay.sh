3582c0ca7a8742bdae9a49fd4ae7b9b7d2aa4cbe
Ben Burke <ben@bernard.francis.burke@gmail.com>
2025-10-10 17:20:01 +1100

    small error on including QEO

--- gDisplay.sh
+++ gDisplay.sh
@@ -198,7 +198,7 @@ edlblend() {
     message "edlblend searching for $SEARCH_STRING and shuffling for $HOW_MANY..."
     # put movies back infind $EDLSRC/ -iname '*.edl' | grep unix | grep -v movies | grep -i "$SEARCH_STRING" > $TMPFILE7
     find $EDLSRC/ -iname '*.edl' | grep unix | grep -i "$SEARCH_STRING" > $TMPFILE7
-    find $QEO/ -iname '*.edl' | grep unix | grep -i "$SEARCH_STRING" > $TMPFILE7
+    find $QEO/ -iname '*.edl' | grep unix | grep -i "$SEARCH_STRING" >> $TMPFILE7
     #find $USCR/ -iname '*.edl' | grep -i "$SEARCH_STRING" >> $TMPFILE7
 
     # if there are no results, exit
